dae1f89b6595dfe975a5e65ecad11a7e5c057d4a
Sven S <info@snowcon.eu>
2017-11-24 01:49:37 -0800

    Added $cordovaGeolocation
    + Geolocation through $cordovaGeolocation
    + Reverse Geocoding through google.maps.Geocoder

--- src/services.js
+++ src/services.js
@@ -3,21 +3,24 @@ angular.module('ion-google-autocomplete')
 
   var autocompleteService = new google.maps.places.AutocompleteService();
   var detailsService = new google.maps.places.PlacesService(document.createElement("input"));
+  var geocoderService = new google.maps.Geocoder();
 
   return {
+
     /**
-     * Search an address from an input and and option country restriction
+     * Search an address from an input and and option country restriction and option place restriction
      * @param required input string
      * @param optional countryCode two letters code
+     * @param optional placeType type from google: (cities) etc
      */
-    searchAddress: function(input, countryCode) {
+    searchAddress: function(input, countryCode, placeType) {
 
       var dfd = $q.defer();
-
       autocompleteService.getPlacePredictions({
-        input: input,
-        componentRestrictions: countryCode ? { country: countryCode } : undefined
-      }, function(result, status) {
+            input,
+            types: placeType ? [placeType] : undefined,
+            componentRestrictions: countryCode ? { country: countryCode } : undefined
+        },function(result, status) {
           
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             
@@ -30,6 +33,7 @@ angular.module('ion-google-autocomplete')
 
       return dfd.promise;
     },
+
     /**
      * Gets the details of a placeId
      * @param required placeId
@@ -44,6 +48,35 @@ angular.module('ion-google-autocomplete')
       });
       
       return dfd.promise;
+    },
+
+    /**
+     * Reverse Geocode the given lat/lng to the location format.
+     * @param required lat float
+     * @param required lng float
+     */
+    reverseGeocode: function(lat, lng) {
+        console.log("Initiating reverseGeocoding");
+
+        var latlng = new google.maps.LatLng(lat,lng);
+        var request = {
+          latLng: latlng
+        };
+        var dfd = $q.defer();
+        document.geoCodeRequestCompleteFlag = 0;
+        geocoderService.geocode(request, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                if (results[0]) {
+                    dfd.resolve(results);
+                } else {
+                    dfd.reject(results);
+                }
+            } else {
+                dfd.reject(status);
+            }
+        });
+
+        return dfd.promise;
     }
   };
 }])
